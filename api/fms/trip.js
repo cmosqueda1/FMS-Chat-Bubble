5a02f1908ef132aa82ca85c2cbe3942cf4c1d98e
mosqueda.christopher <61306146+cmosqueda1@users.noreply.github.com>
2025-12-17 11:08:51 -0600

    Include environment variables in response
    Added environment variable checks to the response.

--- api/fms/trip.js
+++ api/fms/trip.js
@@ -1,8 +1,11 @@
-import { fmsFetch } from "./utils/fmsFetch";
-
 export default function handler(req, res) {
   res.status(200).json({
     ok: true,
-    imported: typeof fmsFetch === "function"
+    env: {
+      FMS_BASE_URL: !!process.env.FMS_BASE_URL,
+      FMS_ACCOUNT: !!process.env.FMS_ACCOUNT,
+      FMS_PASSWORD: !!process.env.FMS_PASSWORD,
+      FMS_COMPANY: !!process.env.FMS_COMPANY
+    }
   });
 }
